111d1559d87855ed1ee7be0dae5725d04a97df06
Prveengurjar397 <gujjarpraveen397@gmail.com>
2026-08-25 11:38:10 +0530

    addedfor_loop.sh to count from 1 to 10
    Added a script that uses a for loop to print numbers from 1 to 10.

---
+++ 90-days_challenge/day-17/for_loop.sh
@@ -0,0 +1,23 @@
+**Count 1 to 10
+
+Used a for loop to print numbers from 1 to 10.
+**
+
+Example
+
+#!/bin/bash
+for i in {1..10}; do
+    echo "$i"
+done
+
+**Output**
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
